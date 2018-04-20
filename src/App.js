41386648508a0713442234ef0fbecff17baeb270
Lam Ngo <lam.ngo.qu@gmail.com>
2018-04-20 09:35:02 +0300

    Modified src files, removed uneccessary files. Modified App.js to fetch the products from Contentful and display them

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import createClient directly
+import {createClient} from 'contentful'
+
+const SPACE_ID = '9hlk44cnhhi9';
+const ACCESS_TOKEN = 'db227854e92bf9f0a7d312a03b7bff4525683f855929ba02262d6f1efce7890e';
+
+const client = createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: SPACE_ID,
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: ACCESS_TOKEN
+})
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    client.getEntries({
+      content_type: 'product'
+    })
+    .then(function(entries) {
+      console.log(entries.items);
+      this.setState({
+        products: entries.items
+      });
+    }.bind(this))
+  }
+
   render() {
+    const productsList =
+      this.state.products.length === 0
+      ? 'loading...'
+      : this.state.products.map((product, index) => <Product key={index} product={product} />);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {productsList}
       </div>
     );
   }
 }
 
+const Product = ({ product }) =>
+  <div>
+    <p>{product.fields.name}</p>
+    <p>From: {product.fields.brand}</p>
+    <p>Image url: {product.fields.image.fields.file.url}</p>
+    <p>{product.fields.description}</p>
+    <button>Add to cart</button>
+    <hr/>
+  </div>
 export default App;
